40854e09ab1f18d9ac421e7495a45f11d81c6eda
NathanHandley <nathanhandley@protonmail.com>
2023-12-26 19:34:18 -0600

    More frame-out

--- src/MasterClassTrainers.cpp
+++ src/MasterClassTrainers.cpp
@@ -1,6 +1,19 @@
 /*
 ** Made by Nathan Handley https://github.com/NathanHandley
 ** AzerothCore 2019 http://www.azerothcore.org/
+*
+* This program is free software; you can redistribute it and/or modify it
+* under the terms of the GNU Affero General Public License as published by the
+* Free Software Foundation; either version 3 of the License, or (at your
+* option) any later version.
+*
+* This program is distributed in the hope that it will be useful, but WITHOUT
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+* FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for
+* more details.
+*
+* You should have received a copy of the GNU General Public License along
+* with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include "Chat.h"
@@ -10,7 +23,22 @@
 #include "ScriptMgr.h"
 #include "World.h"
 
+#include "MasterClassTrainers.h"
+
 using namespace Acore::ChatCommands;
+using namespace std;
+
+static bool ConfigEnabled;
+
+MasterClassTrainersMod::MasterClassTrainersMod()
+{
+
+}
+
+MasterClassTrainersMod::~MasterClassTrainersMod()
+{
+
+}
 
 class MasterClassTrainers_PlayerScript : public PlayerScript
 {
@@ -19,11 +47,17 @@ public:
 
     void OnLogin(Player* player)
     {
+        if (ConfigEnabled == false)
+            return;
+
 	    ChatHandler(player->GetSession()).SendSysMessage("This server is running the |cff4CFF00Master Class Trainer |rmodule.");
     }
 
-    bool CanPrepareGossipMenu(Player* player, WorldObject* source, uint32 menuId /*= 0*/, bool showQuests /*= false*/)
+    bool OnPrepareGossipMenu(Player* player, WorldObject* source, uint32 menuId /*= 0*/, bool showQuests /*= false*/)
     {
+        if (ConfigEnabled == false)
+            return true;
+
         if (Creature* creature = source->ToCreature())
         {
             if (const CreatureTemplate* creatureTemplate = creature->GetCreatureTemplate())
@@ -34,7 +68,8 @@ public:
                     return false;
                 }
             }
-        }        
+        }
+        return true;
     }
 };
 
@@ -45,42 +80,40 @@ public:
 
     void OnAfterConfigLoad(bool /*reload*/) override
     {
-        //if (sConfigMgr->GetOption<bool>("something", true) != somevalue)
-        //{
-        //    do something
-       // }
+        ConfigEnabled = sConfigMgr->GetOption<bool>("MasterClassTrainers.Enable", true);
     }
 };
 
-class MasterClassTrainers_CommandScript : public CommandScript
-{
-public:
-    MasterClassTrainers_CommandScript() : CommandScript("MasterClassTrainers_CommandScript") { }
-
-    ChatCommandTable GetCommands() const override
-    {
-        static ChatCommandTable acaCommandTable =
-        {
-            { "test",     HandleACATestCommand,   SEC_PLAYER, Console::Yes }
-        };
-        static ChatCommandTable commandTable =
-        {
-            { "mct", acaCommandTable }
-        };
-        return commandTable;
-    }
-
-    static bool HandleACATestCommand(ChatHandler* handler, const char* args)
-    {
-        Player* player = handler->GetPlayer();
-        ChatHandler(player->GetSession()).SendSysMessage("This is a test");
-        return true;
-    }
-};
+//class MasterClassTrainers_CommandScript : public CommandScript
+//{
+// TODO: Add command to 'reload from database'
+//public:
+//    MasterClassTrainers_CommandScript() : CommandScript("MasterClassTrainers_CommandScript") { }
+//
+//    ChatCommandTable GetCommands() const override
+//    {
+//        static ChatCommandTable acaCommandTable =
+//        {
+//            { "test",     HandleACATestCommand,   SEC_PLAYER, Console::Yes }
+//        };
+//        static ChatCommandTable commandTable =
+//        {
+//            { "mct", acaCommandTable }
+//        };
+//        return commandTable;
+//    }
+//
+//    static bool HandleACATestCommand(ChatHandler* handler, const char* args)
+//    {
+//        Player* player = handler->GetPlayer();
+//        ChatHandler(player->GetSession()).SendSysMessage("This is a test");
+//        return true;
+//    }
+//};
 
 void AddMasterClassTrainerScripts()
 {
     new MasterClassTrainers_PlayerScript();
     new MasterClassTrainers_WorldScript();
-    new MasterClassTrainers_CommandScript();
+    //new MasterClassTrainers_CommandScript();
 }
