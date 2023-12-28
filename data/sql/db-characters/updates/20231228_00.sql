10cab227cea1bbf840ae96c1d613bb056f6b3d2f
NathanHandley <nathanhandley@protonmail.com>
2023-12-28 15:54:35 -0600

    Add indexes and primary keys

--- data/sql/db-characters/updates/20231228_00.sql
+++ data/sql/db-characters/updates/20231228_00.sql
@@ -1,53 +1,3 @@
-DROP TABLE IF EXISTS `mod_multi_class_character_talent`;
-CREATE TABLE `mod_multi_class_character_talent` (
-	`guid` INT(10) UNSIGNED NOT NULL,
-	`spell` INT(10) UNSIGNED NOT NULL,
-	`class` TINYINT(3) UNSIGNED NOT NULL DEFAULT '0',
-	`specMask` TINYINT(3) UNSIGNED NOT NULL DEFAULT '0',
-	PRIMARY KEY (`guid`, `spell`) USING BTREE
-);
-
-DROP TABLE IF EXISTS `mod_multi_class_character_aura`;
-CREATE TABLE `mod_multi_class_character_aura` (
-	`guid` INT(10) UNSIGNED NOT NULL DEFAULT '0' COMMENT 'Global Unique Identifier',
-	`casterGuid` BIGINT(20) UNSIGNED NOT NULL DEFAULT '0' COMMENT 'Full Global Unique Identifier',
-	`itemGuid` BIGINT(20) UNSIGNED NOT NULL DEFAULT '0',
-	`spell` INT(10) UNSIGNED NOT NULL DEFAULT '0',
-	`effectMask` TINYINT(3) UNSIGNED NOT NULL DEFAULT '0',
-	`class` TINYINT(3) UNSIGNED NOT NULL DEFAULT '0',
-	`recalculateMask` TINYINT(3) UNSIGNED NOT NULL DEFAULT '0',
-	`stackCount` TINYINT(3) UNSIGNED NOT NULL DEFAULT '1',
-	`amount0` INT(10) NOT NULL DEFAULT '0',
-	`amount1` INT(10) NOT NULL DEFAULT '0',
-	`amount2` INT(10) NOT NULL DEFAULT '0',
-	`base_amount0` INT(10) NOT NULL DEFAULT '0',
-	`base_amount1` INT(10) NOT NULL DEFAULT '0',
-	`base_amount2` INT(10) NOT NULL DEFAULT '0',
-	`maxDuration` INT(10) NOT NULL DEFAULT '0',
-	`remainTime` INT(10) NOT NULL DEFAULT '0',
-	`remainCharges` TINYINT(3) UNSIGNED NOT NULL DEFAULT '0',
-	PRIMARY KEY (`guid`, `casterGuid`, `itemGuid`, `spell`, `effectMask`) USING BTREE
-);
-
-DROP TABLE IF EXISTS `mod_multi_class_character_spell`;
-CREATE TABLE `mod_multi_class_character_spell` (
-	`guid` INT(10) UNSIGNED NOT NULL DEFAULT '0' COMMENT 'Global Unique Identifier',
-	`spell` INT(10) UNSIGNED NOT NULL DEFAULT '0' COMMENT 'Spell Identifier',
-	`class` TINYINT(3) UNSIGNED NOT NULL DEFAULT '0',
-	`specMask` TINYINT(3) UNSIGNED NOT NULL DEFAULT '1',
-	PRIMARY KEY (`guid`, `spell`) USING BTREE
-);
-
-DROP TABLE IF EXISTS `mod_multi_class_character_skills`;
-CREATE TABLE `mod_multi_class_character_skills` (
-	`guid` INT(10) UNSIGNED NOT NULL COMMENT 'Global Unique Identifier',
-	`skill` SMALLINT(5) UNSIGNED NOT NULL,
-	`value` SMALLINT(5) UNSIGNED NOT NULL,
-	`class` TINYINT(3) UNSIGNED NOT NULL DEFAULT '0',
-	`max` SMALLINT(5) UNSIGNED NOT NULL,
-	PRIMARY KEY (`guid`, `skill`) USING BTREE
-);
-
 DROP TABLE IF EXISTS `mod_multi_class_characters`;
 CREATE TABLE `mod_multi_class_characters` (
 	`guid` INT(10) UNSIGNED NOT NULL DEFAULT '0' COMMENT 'Global Unique Identifier',
@@ -61,7 +11,60 @@ CREATE TABLE `mod_multi_class_characters` (
 	`resettalents_time` INT(10) UNSIGNED NOT NULL DEFAULT '0',
 	`talentGroupsCount` TINYINT(3) UNSIGNED NOT NULL DEFAULT '1',
 	`activeTalentGroup` TINYINT(3) UNSIGNED NOT NULL DEFAULT '0',
-	PRIMARY KEY (`guid`) USING BTREE
+	PRIMARY KEY (`guid`, `class`) USING BTREE
+);
+
+DROP TABLE IF EXISTS `mod_multi_class_character_talent`;
+CREATE TABLE `mod_multi_class_character_talent` (
+	`guid` INT(10) UNSIGNED NOT NULL,
+	`class` TINYINT(3) UNSIGNED NOT NULL DEFAULT '0',
+	`spell` INT(10) UNSIGNED NOT NULL,
+	`specMask` TINYINT(3) UNSIGNED NOT NULL DEFAULT '0',
+	PRIMARY KEY (`guid`, `class`, `spell`) USING BTREE
+);
+
+DROP TABLE IF EXISTS `mod_multi_class_character_aura`;
+CREATE TABLE `mod_multi_class_character_aura` (
+	`guid` INT(10) UNSIGNED NOT NULL DEFAULT '0' COMMENT 'Global Unique Identifier',
+	`class` TINYINT(3) UNSIGNED NOT NULL DEFAULT '0',
+	`casterGuid` BIGINT(20) UNSIGNED NOT NULL DEFAULT '0' COMMENT 'Full Global Unique Identifier',
+	`itemGuid` BIGINT(20) UNSIGNED NOT NULL DEFAULT '0',
+	`spell` INT(10) UNSIGNED NOT NULL DEFAULT '0',
+	`effectMask` TINYINT(3) UNSIGNED NOT NULL DEFAULT '0',
+	`recalculateMask` TINYINT(3) UNSIGNED NOT NULL DEFAULT '0',
+	`stackCount` TINYINT(3) UNSIGNED NOT NULL DEFAULT '1',
+	`amount0` INT(10) NOT NULL DEFAULT '0',
+	`amount1` INT(10) NOT NULL DEFAULT '0',
+	`amount2` INT(10) NOT NULL DEFAULT '0',
+	`base_amount0` INT(10) NOT NULL DEFAULT '0',
+	`base_amount1` INT(10) NOT NULL DEFAULT '0',
+	`base_amount2` INT(10) NOT NULL DEFAULT '0',
+	`maxDuration` INT(10) NOT NULL DEFAULT '0',
+	`remainTime` INT(10) NOT NULL DEFAULT '0',
+	`remainCharges` TINYINT(3) UNSIGNED NOT NULL DEFAULT '0',
+	PRIMARY KEY (`guid`, `class`, `casterGuid`, `itemGuid`, `spell`, `effectMask`) USING BTREE,
+	INDEX `idx_guidclass` (`guid`, `class`) USING BTREE
+);
+
+DROP TABLE IF EXISTS `mod_multi_class_character_spell`;
+CREATE TABLE `mod_multi_class_character_spell` (
+	`guid` INT(10) UNSIGNED NOT NULL DEFAULT '0' COMMENT 'Global Unique Identifier',
+	`class` TINYINT(3) UNSIGNED NOT NULL DEFAULT '0',
+	`spell` INT(10) UNSIGNED NOT NULL DEFAULT '0' COMMENT 'Spell Identifier',
+	`specMask` TINYINT(3) UNSIGNED NOT NULL DEFAULT '1',
+	PRIMARY KEY (`guid`, `class`, `spell`) USING BTREE,
+	INDEX `idx_guidclass` (`guid`, `class`) USING BTREE
+);
+
+DROP TABLE IF EXISTS `mod_multi_class_character_skills`;
+CREATE TABLE `mod_multi_class_character_skills` (
+	`guid` INT(10) UNSIGNED NOT NULL COMMENT 'Global Unique Identifier',
+	`class` TINYINT(3) UNSIGNED NOT NULL DEFAULT '0',
+	`skill` SMALLINT(5) UNSIGNED NOT NULL,
+	`value` SMALLINT(5) UNSIGNED NOT NULL,
+	`max` SMALLINT(5) UNSIGNED NOT NULL,
+	PRIMARY KEY (`guid`, `class`, `skill`) USING BTREE,
+	INDEX `idx_guidclass` (`guid`, `class`) USING BTREE
 );
 
 DROP TABLE IF EXISTS `mod_multi_class_next_switch_class`;
