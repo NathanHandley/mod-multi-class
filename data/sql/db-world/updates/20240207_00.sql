945def30b68efe68b2eca3c4cd1c99f58f4e3c6d
NathanHandley <nathanhandley@protonmail.com>
2024-02-07 18:47:56 -0600

    Correct Spell ID in master skill spell list for Metamorphosis

--- data/sql/db-world/updates/20240207_00.sql
+++ data/sql/db-world/updates/20240207_00.sql
@@ -1,2 +1,3 @@
 -- Update Metamorphosis SpellID
 UPDATE mod_multi_class_spells SET SpellID = 47241 WHERE SpellID = 59672;
+UPDATE mod_multi_class_master_skill_spells SET LearnSpellID = 47241 WHERE LearnSpellID = 59672;
